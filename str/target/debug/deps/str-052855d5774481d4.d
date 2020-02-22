c0c228651a3cacb1523b710f0a4366f1a05fb76d
Vlad <accierro@gmail.com>
2020-02-22 17:14:34 +0000

    Cleaning
    Add gitignore, delete all redundant files

--- str/target/debug/deps/str-052855d5774481d4.d
+++
@@ -1,5 +0,0 @@
-/Users/accierro/Repos/Sandbox/Rust/book_tasks/str/target/debug/deps/str-052855d5774481d4: src/main.rs
-
-/Users/accierro/Repos/Sandbox/Rust/book_tasks/str/target/debug/deps/str-052855d5774481d4.d: src/main.rs
-
-src/main.rs:
--- str/target/debug/str.d
+++
@@ -1,1 +0,0 @@
-/Users/accierro/Repos/Sandbox/Rust/book_tasks/str/target/debug/str: /Users/accierro/Repos/Sandbox/Rust/book_tasks/str/src/main.rs
