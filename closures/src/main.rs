993e7ebccc7f7e11697d71ebb7fd6d404353751c
Vlad <accierro@gmail.com>
2020-03-24 16:24:12 +0000

    Update minigrep. Add Closures

---
+++ closures/src/main.rs
@@ -0,0 +1,37 @@
+// To define a closure inside main struct we use trait bound Fn
+// CLosure must implement one of the traits Fn, FnMut, FnOnce
+// value is Option because before executing a closure value is going to be None
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(val) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
